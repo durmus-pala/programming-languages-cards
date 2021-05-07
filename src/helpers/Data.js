eb4216956b5334a5c65b4536a394a9bec1cbcdfb
Durmuş PALA <71263028+durmus-pala@users.noreply.github.com>
2021-05-07 12:02:36 +0300

    first commit

---
+++ src/helpers/Data.js
@@ -0,0 +1,51 @@
+import css from "../helpers/css.png";
+import python from "../helpers/python.png";
+import java from "../helpers/java.png";
+import cpp from "../helpers/cpp.png";
+import go from "../helpers/go.png";
+import php from "../helpers/php.png";
+import javascript from "../helpers/javascript.png";
+import html from "../helpers/html.png";
+
+export const categories = [
+  {
+    name: "javascript",
+    img: javascript,
+    options: ["Website Development", "1995", "Brendan Eich"],
+  },
+  {
+    name: "html",
+    img: html,
+    options: ["Website Development", "1993", "Tim Berners-Lee"],
+  },
+  {
+    name: "css",
+    img: css,
+    options: ["Website Design", "1994", "Håkon Wium Lie"],
+  },
+  {
+    name: "python",
+    img: python,
+    options: ["AI", "1980", "Guido van Rossum"],
+  },
+  {
+    name: "java",
+    img: java,
+    options: ["BackEnd", "1990s", "Sun Microsystems"],
+  },
+  {
+    name: "c++",
+    img: cpp,
+    options: ["Game Dev.", "1980s", "Bjarne Stroustrup"],
+  },
+  {
+    name: "go",
+    img: go,
+    options: ["Network", "2009", "Robert Griesemer"],
+  },
+  {
+    name: "php",
+    img: php,
+    options: ["BackEnd", "1995", "	Rasmus Lerdorf"],
+  },
+];
